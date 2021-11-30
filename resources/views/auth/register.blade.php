9f8c047406c9d34d3a5a784159ac9c17bbc9d353
TAMEN <selouatamen@yahoo.fr>
2021-11-30 14:40:26 +0100

    commit sel

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,31 +1,31 @@
 
     <!DOCTYPE html>
     <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    
+
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-    
+
         <!-- CSRF Token -->
         <meta name="csrf-token" content="{{ csrf_token() }}">
-    
+
         <title>{{ config('app.name', 'Laravel') }}</title>
-    
+
         <!-- Scripts -->
         <script src="{{ asset('js/app.js') }}" defer></script>
-    
+
         <!-- Fonts -->
         <link rel="dns-prefetch" href="//fonts.gstatic.com">
         <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
-    
+
         <!-- Styles -->
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
         <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-    
+
     </head>
 <body>
-    
-<h1 class="text-3xl text-green-500 mb-6 text-center">Completez vos informations</h1>
+
+<h1 class="title text-3xl text-500 mb-6 text-center style="margin-top:69px">Completez vos informations</h1>
 <form method="POST" action="{{ route('register') }}" class="w-full max-w-sm mx-auto rounded-lg border shadow-md p-5 mb-5">
     @csrf
     <div class="mb-4">
@@ -58,9 +58,44 @@
     </div>
 
 
-    <button type="submit" class="bg-green-500 text-white hover:bg-green-700 transition ease-in-out duration-500 rounded-md shadow-md w-full block px-4 py-2 mt-3">Créer mon compte</button>
+    <button type="submit" class="button1">Créer mon compte</button>
 </form>
 
 </body>
 
 </html>
+
+<style>
+
+.button1
+{
+    background-color: #373737;
+    color: white;
+    border-radius: 50% 20% / 10% 40%
+
+}
+
+.button1
+{
+    border: none;
+    color: white;
+    padding: 15px 32px;
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+    font-size: 16px;
+    margin: 4px 2px;
+    cursor: pointer;
+}
+
+.button1:hover{
+
+    color: white;
+
+}
+h1 .title{
+    color: #373737;
+    margin-top: 30px;
+}
+
+</style>
